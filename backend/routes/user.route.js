ba17ad7b280bfb1daa5022eece1491c1198fe557
Sagar1079 <sagarwankhade896@gmail.com>
2023-02-23 00:08:19 +0530

    Sign In with Password Encryption

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-const {UserModel} = require("../models/user.model");
+const { UserModel } = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const userRouter = Router();
@@ -9,15 +10,40 @@ userRouter.post("/signIn", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    bcrypt.hash(password, 3, async (err, hash) => {
+    bcrypt.hash(password, process.env.SaltRounds, async (err, hash) => {
       if (err) {
-        res.send("Error Occurred, Please try again 1");
+        res.send("Error Occurred, Please try again");
       } else {
         const newUser = new UserModel({ email, password: hash });
         await newUser.save();
         res.send("Account created successfully");
       }
     });
+  } catch (err) {
+    res.send("Error Occurred, Please try again");
+  }
+});
+
+
+
+
+userRouter.post("/logIn", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const findUser = await UserModel.findOne({ email });
+    if (findUser) {
+      bcrypt.compare(password, findUser.password, function (err, result) {
+        if (result) {
+          const token = jwt.sign({ key: email }, process.env.encryption);
+          res.send(token);
+        } else {
+          res.send("Wrong Password");
+        }
+      });
+    } else {
+      res.send("Email not Found");
+    }
   } catch (err) {
     res.send("Error Occurred, Please try again");
   }
